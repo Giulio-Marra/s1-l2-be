31538cfb864e39ae4435f9158664f220d3fb4e34
Giulio-Marra <giuliomarramail@gmail.com>
2024-05-21 16:44:44 +0200

    Update Main.java
    esercizio 1

--- si-l2/src/Esercizio1/Main.java
+++ si-l2/src/Esercizio1/Main.java
@@ -16,12 +16,18 @@ public class Main {
     }
 
     public static boolean annoBisestile(int year) {
-        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-            return true;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
         } else {
             return false;
         }
-
     }
-    
 }
