2e362d850456826588028bdf73e439692de44755
Giulio-Marra <giuliomarramail@gmail.com>
2024-05-22 13:45:53 +0200

    aa

--- si-l2/src/Esercizio1/Main.java
+++ si-l2/src/Esercizio1/Main.java
@@ -3,7 +3,9 @@ package Esercizio1;
 public class Main {
     public static void main(String[] arg) {
         System.out.println(stringaPariDispari("4"));
-        System.out.println(annoBisestile(2028));
+        System.out.println(annoBisestile(2018));
+        System.out.println(annoBisestileInLine(2018));
+        System.out.println(annoBisestileInLine3(2018));
     }
 
     public static boolean stringaPariDispari(String str) {
@@ -26,6 +28,24 @@ public class Main {
             } else {
                 return true;
             }
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean annoBisestileInLine(int years) {
+        if (years % 4 == 0 && years % 100 == 0 && years % 400 == 0 || years % 4 == 0 && years % 100 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean annoBisestileInLine3(int years) {
+        if (years % 4 == 0 && years % 100 != 0) {
+            return true;
+        } else if (years % 4 == 0 && years % 100 == 0 && years % 400 == 0) {
+            return true;
         } else {
             return false;
         }
